2a822efdce0efeada3c1a366fe70022c0ac051e2
Hawk <tsasha1208@rambler.ru>
2021-12-04 16:16:12 +0300

    Initial commit

---
+++ src/main/java/ru/timofeev/butchery/ButcheryApplication.java
@@ -0,0 +1,13 @@
+package ru.timofeev.butchery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButcheryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButcheryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/timofeev/butchery/ButcheryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.timofeev.butchery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButcheryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
