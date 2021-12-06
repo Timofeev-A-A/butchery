ab08364feab2abbbac360382705ebd5e086b8b7b
Hawk <tsasha1208@rambler.ru>
2021-12-06 19:02:52 +0300

    Исправлены ошибки в документах, проект способен подключаться к БД

--- src/main/java/ru/timofeev/butchery/ButcheryApplication.java
+++ src/main/java/ru/timofeev/butchery/ButcheryApplication.java
@@ -2,11 +2,18 @@ package ru.timofeev.butchery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @SpringBootApplication
+@Controller
 public class ButcheryApplication {
+
+	@GetMapping("/")
+	public String index(){
+		return "index";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ButcheryApplication.class, args);
